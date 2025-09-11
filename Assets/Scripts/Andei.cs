34f05ee13a88824588cfe5472159d99c63148671
“Heribas” <joaoh.santana07@gmail.com>
2025-09-11 09:04:16 -0300

    Mudei o ababababa para o start do script andei

--- Assets/Scripts/Andei.cs
+++ Assets/Scripts/Andei.cs
@@ -5,12 +5,12 @@ public class Andei : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("ababababa");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("ababababa");
+    
     }
 }
